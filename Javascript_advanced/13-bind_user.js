fd7f2a310fa789ce71149bedc8cd4c0fe74e04b4
apsamade <bouderga.abdessamade@gmail.com>
2026-05-22 15:33:17 +0200

    update 13

--- Javascript_advanced/13-bind_user.js
+++ Javascript_advanced/13-bind_user.js
@@ -1,19 +1,16 @@
 const user = {
-
-    hobby: "Calligraphy",
-    favoriteSport: "Hockey",
-    astrologicalSign: "Aries",
-    firstName: "Buillaume",
-    lastName: "Ialva",
-    location: "Telaviv",
-    occupation: "Engineer",
-
-logWelcomeUser: function(welcomeString){
-            console.log(welcomeString + ', ' + this.firstName + ". Your occupation is: " + this.occupation);
-
-    }
-
+  hobby: 'Calligraphy',
+  favoriteSport: 'Hockey',
+  astrologicalSign: 'Aries',
+  firstName: 'Guillaume',
+  lastName: 'Johns',
+  location: 'Netherlands',
+  occupation: 'Engineer',
 };
 
-const bindLogWelcomeUser = user.logWelcomeUser.bind(user);
-bindLogWelcomeUser('Hello');
+function logWelcomeUser(welcomeString) {
+  console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('Welcome');
