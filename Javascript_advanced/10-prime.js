1f89f3820e86e0b5ebf3ab516baf22afbc1c1c0e
apsamade <bouderga.abdessamade@gmail.com>
2026-05-22 15:29:39 +0200

    update 10

--- Javascript_advanced/10-prime.js
+++ Javascript_advanced/10-prime.js
@@ -1,28 +1,24 @@
-function isPrime(num) {
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return num > 1;
-}
-
 function countPrimeNumbers() {
-    let count = 0;
-    for (let i = 2; i <= 100; i++) {
-        if (isPrime(i)) {
-            count++;
-        }
+  let count = 0;
+
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
     }
-    return count;
+    return true;
+  }
+
+  for (let num = 2; num <= 100; num++) {
+    if (isPrime(num)) count++;
+  }
+  return count;
 }
 
-const startTime = performance.now();
+const start = performance.now();
 for (let i = 0; i < 100; i++) {
-    countPrimeNumbers();
+  countPrimeNumbers();
 }
-const endTime = performance.now();
+const end = performance.now();
 
-const timeUsed = endTime - startTime;
-
-console.log('Execution time of printing countPrimeNumbers was ' + timeUsed + ' milliseconds.');
+console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`);
